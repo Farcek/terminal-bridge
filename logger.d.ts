4201eab9ac3f0708297933f6ceb6968b25f6f7d3
Farcek <farcek@gmail.com>
2018-06-08 10:46:21 +0800

    logger add genToken function

--- logger.d.ts
+++ logger.d.ts
@@ -1,6 +1,7 @@
 declare namespace terminal {
 
     export module logger {
+        export function genToken(owner: string): string;
         export const Appid: number;
         export const Anchar: string;
         export const Token: string;
